d47e73953d69d2706e55b3ec96fe9f4e6649d19c
Fahmisutansyah <fahmisutansyah@gmail.com>
2019-02-12 12:53:12 +0700

    exer 1

---
+++ exercise-1.js
@@ -0,0 +1,10 @@
+function angkaPrima(angka) {
+    for(var i = 2; i < angka; i++)
+      if(angka % i === 0) return false;
+    return angka !== 1 && angka !== 0;
+  }
+  console.log(angkaPrima(3)); // true
+  console.log(angkaPrima(7)); // true
+  console.log(angkaPrima(6)); // false
+  console.log(angkaPrima(23)); // true
+  console.log(angkaPrima(33)); // false
