d4f8d66df58f6057477e91ee14e8101351131a2f
Fahmisutansyah <fahmisutansyah@gmail.com>
2019-02-14 21:57:32 +0700

    exer 7

---
+++ exercise-7.js
@@ -0,0 +1,19 @@
+function urutkanAbjad(string){
+    var alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    var output = ''
+    for (let i = 0; i < alphabet.length; i++){
+        for (let j = 0; j < string.length; j++){
+            if (alphabet[i] === string[j]){
+                output += alphabet[i]
+            }
+        }
+    }
+    return output
+}
+
+// TEST CASES
+console.log(urutkanAbjad('hello')); // 'ehllo'
+console.log(urutkanAbjad('truncate')); // 'acenrttu'
+console.log(urutkanAbjad('developer')); // 'deeeloprv'
+console.log(urutkanAbjad('software')); // 'aeforstw'
+console.log(urutkanAbjad('aegis')); // 'aegis'
